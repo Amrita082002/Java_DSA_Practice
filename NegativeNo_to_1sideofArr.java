21c81b82a2f4170ef40ff6a63ea410dc0dee2631
Amrita082002 <amritapal082002@gmail.com>
2026-05-23 14:39:34 +0530

    Added the code

---
+++ NegativeNo_to_1sideofArr.java
@@ -0,0 +1,41 @@
+package arrays;
+
+import java.util.*;
+
+public class NegativeNo_to_1sideofArr{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int left = 0;
+        int right = n - 1;
+
+        while(left <= right) {
+
+            if(arr[left] < 0) {
+                left++;
+            }
+            else if(arr[right] > 0) {
+                right--;
+            }
+            else {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
